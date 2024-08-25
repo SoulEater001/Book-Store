2779a1f43d5b36e1183deb4fc75cbe720d53e0c3
SoulEater001 <dshivam698@gmail.com>
2024-08-25 20:31:59 +0530

    SPA and Added react-router-dom

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import CreateBook from './pages/CreateBook';
+import DeleteBook from './pages/DeleteBook';
+import Home from './pages/Home';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white' >
-      App
-    </div>
-  )
-}
+    <Routes>
+      <Route path='/' element={<Home />} ></Route>
+      <Route path='' element={<CreateBook />} ></Route>
+      <Route path='' element={<ShowBook />} ></Route>
+      <Route path='' element={<EditBook />} ></Route>
+      <Route path='' element={<DeleteBook />} ></Route>
+    </Routes>
+  );
+};
 
 export default App
