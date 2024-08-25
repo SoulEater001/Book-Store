bdad70ec4ee18237cdc443e0b04582f8fdc38c15
SoulEater001 <dshivam698@gmail.com>
2024-08-25 20:41:49 +0530

    Fixed Route for pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,10 +10,10 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} ></Route>
-      <Route path='' element={<CreateBook />} ></Route>
-      <Route path='' element={<ShowBook />} ></Route>
-      <Route path='' element={<EditBook />} ></Route>
-      <Route path='' element={<DeleteBook />} ></Route>
+      <Route path='/books/create' element={<CreateBook />} ></Route>
+      <Route path='/books/details/:id' element={<ShowBook />} ></Route>
+      <Route path='/books/edit/:id' element={<EditBook />} ></Route>
+      <Route path='/books/delete/:id' element={<DeleteBook />} ></Route>
     </Routes>
   );
 };
